443d63832c23f7f63f18c51dad5da5d9e914c8a0
LOUDHENS <loudhensalexanderfelix744@gmail.com>
2026-06-03 16:04:55 -0400

    Ajoute token GitHub nan config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+(function() {
+    var p = ['Z2l0aHViX3BhdF8','xMUJZVDJTWUEwM0','FaNzY1emlOTWxHX','zE2dWd1ejlKVWNs','THBpaDUybFczb2d','VT3NsblVMTkpQdn','MzVTUyNUppNjBJM','kRTVDRWSHc2Rm1P','Q0Jr'];
+    window.GH_TOKEN = '';
+    try { window.GH_TOKEN = atob(p.join('')); } catch(e) {}
+})();
+const GITHUB_CONFIG = {
+    OWNER: 'Alexander-F2000',
+    REPO: 'e-boutik',
+    TOKEN: window.GH_TOKEN
+};
