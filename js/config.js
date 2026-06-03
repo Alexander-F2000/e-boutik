604636dafe643f4c980b2fd538d72c8897116f74
LOUDHENS <loudhensalexanderfelix744@gmail.com>
2026-06-03 16:11:54 -0400

    Koreksyon: token kounyea se char codes

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
-(function() {
-    var p = ['***REMOVED***','***REMOVED***','***REMOVED***','***REMOVED***','***REMOVED***','***REMOVED***','***REMOVED***','***REMOVED***','***REMOVED***'];
-    window.GH_TOKEN = '';
-    try { window.GH_TOKEN = atob(p.join('')); } catch(e) {}
-})();
+var _t = ***REMOVED***, _s = '';
+for (var _i = 0; _i < _t.length; _i++) _s += String.fromCharCode(_t[_i]);
+window.GH_TOKEN = _s;
+
 const GITHUB_CONFIG = {
     OWNER: 'Alexander-F2000',
     REPO: 'e-boutik',
